97c467e6f0c35c8a8b6558296d09d70bc639fa1f
pappijai <johnintia30@gmail.com>
2022-04-18 15:00:38 +0800

    integrate currency rate module

---
+++ pages/currency-rates/_slug/update.vue
@@ -0,0 +1,104 @@
+<template>
+	<div id="dashboard" v-if="loaded">
+
+		<div class="actions">
+			<nuxt-link to="/currency-rates" class="cancel button pointer">
+				<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
+				<span>Back</span>
+			</nuxt-link>
+		</div>
+
+		<ValidationObserver tag="div" ref="form">
+			<form id="form" class="form mb seventy" @submit.prevent="submit()">
+				<div class="box mb">
+					<div class="top_box">
+						<h2>Information</h2>
+					</div>
+					<div class="bottom_box">
+						<ValidationProvider tag="div" class="group bordered" name="name" :rules="{ required: true }" v-slot="{ errors }">
+							<label for="name">Name *</label>
+							<input type="text" class="input" name="name" autocomplete="off" placeholder="Enter company name" v-model="form_data.name">
+							<transition name="slide"><span class="validate" v-if="errors.length > 0">{{ errors[0] }}</span></transition>
+						</ValidationProvider>
+						<ValidationProvider tag="div" class="group bordered" name="Amount per 1 USD" :rules="{ required: true }" v-slot="{ errors }">
+							<label for="amount">Amount per 1 USD (2 decimals only)*</label>
+							<input type="number" step="0.01" class="input" name="amount" autocomplete="off" placeholder="Enter amount per 1 USD" v-model="form_data.amount">
+							<transition name="slide"><span class="validate" v-if="errors.length > 0">{{ errors[0] }}</span></transition>
+						</ValidationProvider>
+					</div>
+				</div>
+				<div class="buttons fixed">
+					<nuxt-link to="/" class="cancel button half_width btn lg">Cancel</nuxt-link>
+					<button type="submit" class="success button half_width btn lg pointer">Submit</button>
+				</div>
+			</form>
+		</ValidationObserver>
+	</div>
+</template>
+
+<script>
+	export default {
+		data: ({ $moment }) => ({
+			loaded: false,
+			form_data: {
+				name: null,
+				amount: null,
+			},
+		}),
+		methods: {
+			submit () {
+				const me = this
+				me.$refs.form.validate().then(success => {
+					if (!success) {
+						me.$scrollTo('.validate', {
+							offset: -250
+						})
+						return
+					} else {
+						me.toggleModalStatus({ type: 'loader', status: true })
+						let form_data = new FormData()
+
+						form_data.append('name', me.form_data.name)
+						form_data.append('amount', me.form_data.amount)
+
+						me.$axios.post(`admin/currency-rate/update/${me.$route.params.slug}`, form_data).then(res => {
+							me.$store.dispatch('global/toast/addToast', { type: 'success', message: 'Item has been updated!' })
+							me.$nuxt.refresh()
+						}).catch(err => {
+							me.toggleModalStatus({ type: 'catcher', status: true, item: { errors: err.response.data.errors } })
+						}).then(() => {
+							setTimeout( () => {
+								me.toggleModalStatus({ type: 'loader', status: false })
+							}, 500)
+						})
+					}
+					me.$nextTick(() => {
+						me.$refs.form.reset()
+					})
+				})
+			}
+		},
+		mounted () {
+			const me = this
+			me.toggleModalStatus({ type: 'loader', status: true })
+			setTimeout( () => {
+				me.toggleModalStatus({ type: 'loader', status: false })
+				me.loaded = true
+			}, 500)
+		},
+		asyncData ({ $axios, store, params }) {
+			store.commit('global/settings/populateTitle', { title: 'Currency Rates' })
+
+			return $axios.$get(`admin/currency-rate/info/${params.slug}`).then(({ res }) => {
+				return {
+					form_data: {
+						name: res.name,
+						amount: res.amount,
+					}
+				}
+			}).catch(({ response: { data: { errors } } }) => {
+				store.commit('global/modal/toggleModalStatus', { type: 'catcher', status: true, item: { errors: errors } })
+			})
+		}
+	}
+</script>
--- store/global/sidebar/state.js
+++ store/global/sidebar/state.js
@@ -15,6 +15,11 @@ export default () => ({
 			slug: '/country',
 			icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z"></path><line x1="4" y1="22" x2="4" y2="15"></line></svg>',
 		},
+		{
+			name: 'Currency Rates',
+			slug: '/currency-rates',
+			icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><line x1="12" y1="1" x2="12" y2="23"></line><path d="M17 5H9.5a3.5 3.5 0 0 0 0 7h5a3.5 3.5 0 0 1 0 7H6"></path></svg>',
+		},
 	],
 	toggled: false
 })
