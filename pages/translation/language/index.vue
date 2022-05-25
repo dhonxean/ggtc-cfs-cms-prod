f156052808ea48f247e7aff66ba992136e26e18d
pappijai <johnintia30@gmail.com>
2022-05-25 11:26:41 +0800

    remove english delete funciton

--- pages/translation/language/index.vue
+++ pages/translation/language/index.vue
@@ -96,10 +96,12 @@
 								<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
 								<span>Edit</span>
 							</nuxt-link>
-							<div class="item ml cancel pointer" @click="toggleModalStatus({ type: 'delete_confirmation', status: true, item: { api: `v2/admin/language/delete/${data.id}`, item_type: 'language' } })">
-								<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
-								<span>Delete</span>
-							</div>
+							<template v-if="data.sequence != 1">
+								<div class="item ml cancel pointer" @click="toggleModalStatus({ type: 'delete_confirmation', status: true, item: { api: `v2/admin/language/delete/${data.id}`, item_type: 'language' } })">
+									<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
+									<span>Delete</span>
+								</div>
+							</template>
 						</div>
 					</td>
 				</tr>
@@ -169,8 +171,7 @@
 		asyncData ({ $axios, store }) {
 			store.commit('global/settings/populateTitle', { title: 'Language' })
 			let form_data = {
-				sort_by: 'name',
-				order_type: 'asc'
+				first_load: true
 			}
 			return $axios.$post('v2/admin/language/get-all-language', form_data).then(({ res }) => {
 				return {
