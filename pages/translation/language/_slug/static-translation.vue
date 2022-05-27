8fdac51e011ea9fa9c68e36f237b57db9e597da6
pappijai <johnintia30@gmail.com>
2022-05-27 18:12:15 +0800

    add image viewer

--- pages/translation/language/_slug/static-translation.vue
+++ pages/translation/language/_slug/static-translation.vue
@@ -35,7 +35,7 @@
 									<div class="field-input pt20">
 										<span>{{ item }}</span>
 										<div>
-											<div class="success button pointer">
+											<div class="success button pointer" @click="toggleModalStatus({ type: 'image_viewer', status: true, item: { path: key } })">
 												<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"></path><circle cx="12" cy="12" r="3"></circle></svg>
 												<span>View this part</span>
 											</div>
