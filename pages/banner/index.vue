3053d54bf52a61c33b2bd3af9d6e29c94436239c
pappijai <johnintia30@gmail.com>
2022-07-19 10:24:50 +0800

    add banner module

--- pages/banner/index.vue
+++ pages/banner/index.vue
@@ -4,7 +4,7 @@
 		<div class="actions">
 			<nuxt-link to="/banner/create" class="success button pointer">
 				<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-				<span>Add Company</span>
+				<span>Add Banner</span>
 			</nuxt-link>
 		</div>
 
@@ -58,7 +58,7 @@
 								<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><path d="M11 4H4a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-7"></path><path d="M18.5 2.5a2.121 2.121 0 0 1 3 3L12 15l-4 1 1-4 9.5-9.5z"></path></svg>
 								<span>Edit</span>
 							</nuxt-link>
-							<div class="item ml cancel pointer" @click="toggleModalStatus({ type: 'delete_confirmation', status: true, item: { api: `admin/banner/delete/${data.id}`, item_type: 'banner' } })">
+							<div class="item ml cancel pointer" @click="toggleModalStatus({ type: 'delete_confirmation', status: true, item: { api: `v1/admin/banner/delete/${data.id}`, item_type: 'banner' } })">
 								<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" class="icon"><polyline points="3 6 5 6 21 6"></polyline><path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2"></path></svg>
 								<span>Delete</span>
 							</div>
@@ -104,7 +104,7 @@
 				sort_by: 'name',
 				order_type: 'asc'
 			}
-			return $axios.$get('admin/banner/list', form).then(({ res }) => {
+			return $axios.$get('v1/admin/banner/list', form).then(({ res }) => {
 				return {
 					records: res
 				}
--- store/global/sidebar/state.js
+++ store/global/sidebar/state.js
@@ -5,11 +5,11 @@ export default () => ({
 			slug: '/dashboard',
 			icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#FF9000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><path d="M3 9l9-7 9 7v11a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"></path><polyline points="9 22 9 12 15 12 15 22"></polyline></svg>'
 		},
-		// {
-		// 	name: 'Banner',
-		// 	slug: '/banner',
-		// 	icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#FF9000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>',
-		// },
+		{
+			name: 'Banner',
+			slug: '/banner',
+			icon: '<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#FF9000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="icon"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><circle cx="8.5" cy="8.5" r="1.5"></circle><polyline points="21 15 16 10 5 21"></polyline></svg>',
+		},
 		{
 			name: 'Company',
 			slug: '/company',
