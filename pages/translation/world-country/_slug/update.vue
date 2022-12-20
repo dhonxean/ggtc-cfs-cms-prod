435f674b7c92e1a3f9d5404ba8ba7a69e1f4c54c
pappijai <johnintia30@gmail.com>
2022-12-20 18:23:34 +0800

    CMS MASTER: remove button of add in update world country

--- pages/translation/world-country/_slug/update.vue
+++ pages/translation/world-country/_slug/update.vue
@@ -6,10 +6,6 @@
 				<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="19" y1="12" x2="5" y2="12"></line><polyline points="12 19 5 12 12 5"></polyline></svg>
 				<span>Back</span>
 			</nuxt-link>
-			<nuxt-link to="/translation/world-country/create" class="success ml ten button pointer">
-				<svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" viewBox="0 0 24 24" fill="none" stroke="#fff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="12" y1="5" x2="12" y2="19"></line><line x1="5" y1="12" x2="19" y2="12"></line></svg>
-				<span>Add Another</span>
-			</nuxt-link>
 		</div>
 
 		<ValidationObserver tag="div" ref="form">
